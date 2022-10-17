15a9496848cbc1d773c1ce48e479aef1772e639b
evansaeger <111322168+evansaeger@users.noreply.github.com>
2022-10-16 19:55:22 -0500

    Add files via upload

---
+++ L08Unit2Exam.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace L08Unit2Exam;
+
+
+Console.WriteLine "Enter a number between 1 and 5";
+input "Number";
+
+{
+if (num == 1) print("I");
+if (num == 2) print("II");
+if (num == 3) print ("III");
+if (num == 4) print ("IV");
+if (num == 5) print ("V");
+else: print("ERROR!");
+
+}
